19fc1331b2b4acad10c7a087bbc825d5f21031c0
tryserverless <124862144+tryserverless@users.noreply.github.com>
2023-02-09 15:28:03 +0000

    Add composer's autoload

--- api/index.php
+++ api/index.php
@@ -2,10 +2,12 @@
 
 declare(strict_types=1);
 
+require_once __DIR__.'/../vendor/autoload.php';
+
 use tryserverless\Api\BottlePack;
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $bottles = $_POST["bottles"];
+    $bottles = (int) $_POST["bottles"];
     $packSizes = $_POST["packSizes"];
     $result = (new BottlePack())->calculateBottlePacks($bottles, $packSizes);
 
